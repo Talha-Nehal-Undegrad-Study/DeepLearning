e5cbbc4c54f904f2d9b1fb87722f9f290a28da59
Talha Ahmed <122668359+TalhaAhmed2000@users.noreply.github.com>
2023-07-12 20:52:05 +0500

    Update utils.py

--- Task 1/python_scripts/utils.py
+++ Task 1/python_scripts/utils.py
@@ -1,5 +1,48 @@
 
 import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import os
+
+def create_writer(experiment_name: str, 
+                  model_name: str, 
+                  extra: str = None) -> torch.utils.tensorboard.writer.SummaryWriter():
+    """Creates tensorboard and saves it to a specified log directory.
+
+    log directory is a combination of runs/timestamp/experiment_name/model_name/extra.
+
+    Where timestamp is the current date in YYYY-MM-DD format.
+
+    Args:
+        experiment_name (str): Name of experiment.
+        model_name (str): Name of model.
+        extra (str, optional): Anything extra to add to the directory. Defaults to None.
+
+    Returns:
+        torch.utils.tensorboard.writer.SummaryWriter(): Instance of a writer saving to log_dir.
+
+    Example usage:
+        # Create a writer saving to "runs/2022-06-04/pet_breed_80_20_split/AnimalBreedClassifierV0/5_epochs/"
+        writer = create_writer(experiment_name = "pet_breed_80_20_split",
+                               model_name = "AnimalBreedClassifierV0",
+                               extra="5_epochs")
+        # The above is the same as:
+        writer = SummaryWriter(log_dir="runs/2022-06-04/data_10_percent/effnetb2/5_epochs/")
+    """
+
+    # Get timestamp of current date 
+    timestamp = datetime.now().strftime("%Y-%m-%d")
+
+    if extra:
+        # Create log directory path
+        log_dir = os.path.join("content/model_runs", timestamp, experiment_name, model_name, extra)
+    else:
+        log_dir = os.path.join("content/model_runs", timestamp, experiment_name, model_name)
+        
+    print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
+    return SummaryWriter(log_dir = log_dir)
+
 
 # Defining a function to print the time taken to train a model
 def time_taken(start, end):
