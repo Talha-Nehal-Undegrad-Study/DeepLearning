7b84dcb5f14c4343d6bf428a3642967af2f69200
Talha Ahmed <24100033@lums.edu.pk>
2023-07-12 20:14:53 +0500

    add utils

---
+++ Task 1/python_scripts/utils.py
@@ -0,0 +1,26 @@
+
+import torch
+import numpy as np
+
+# Python script for helpful functions we will be needing throughtout these tasks
+
+def multiclass_f1score(prec: torch.Tensor, recall: torch.Tensor) -> torch.float:
+  return (2 * prec * recall)/(prec + recall)
+
+# Defining a function to print the time taken to train a model
+def time_taken(start, end):
+  print(f'\nTrain Time: {end -start:.3f} seconds')
+
+# Defining functions for IOU and Dice coefficient metrics to evaluate the quality of mask segmentation in Task 2
+
+def iou(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    union = np.sum(pred_mask) + np.sum(groundtruth_mask) - intersect
+    iou = np.mean(intersect/union)
+    return round(iou, 3)
+
+def dice_coef(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    total_sum = np.sum(pred_mask) + np.sum(groundtruth_mask)
+    dice = np.mean(2*intersect/total_sum)
+    return round(dice, 3) #round up to 3 decimal places
